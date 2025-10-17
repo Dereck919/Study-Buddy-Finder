7c1856410f1d80f5fde76ad700084b603b0b46c9
DjRePrincess <148670490+DjRePrincess@users.noreply.github.com>
2025-10-17 11:07:34 -0700

    404 page, separated code from server.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import express from 'express';
+
+const app = express();
+
+const dbURI = "mongodb+srv://DjRePrincess:test1234@cluster0.aaph9xc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(dbURI)
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err)) ; 
+
+app.set('view engine', 'ejs');
+
+// listings
+
+
+//404 page 
+app.use((req, res) => {
+    res.status(404).render('404');
+});
